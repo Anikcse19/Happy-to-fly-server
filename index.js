2b9d8602bf37c4ec62b3f35cd7d62e2a73d6f366
Anikcse19 <anikdebcse@gmail.com>
2021-10-31 12:15:22 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient } = require('mongodb');
+const ObjectId = require("mongodb").ObjectId;
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1qogd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+    const packagesCollection = client.db("Travel-Agency").collection("packages");
+    const ordersCollection = client.db("Travel-Agency").collection('Orders')
+
+    // get all packages 
+    app.get('/homepackages', async (req, res) => {
+
+        const result = await packagesCollection.find({}).toArray()
+        res.send(result)
+
+        console.log('hello')
+    })
+
+    // get single package 
+    app.get('/ordered/:id', async (req, res) => {
+        const result = await packagesCollection.find({ _id: ObjectId(req.params.id) }).toArray()
+        res.send(result)
+    })
+
+    // get my orders 
+    app.get('/orders', async (req, res) => {
+        const result = await ordersCollection.find({}).toArray()
+        res.send(result)
+
+    })
+    // cancel order
+    app.delete("/cancelOrder/:id", async (req, res) => {
+        console.log(req.params.id);
+
+        ordersCollection
+            .deleteOne({ _id: ObjectId(req.params.id) })
+            .then((result) => {
+                res.send(result);
+            });
+    });
+
+    // ordered package post 
+    app.post('/myorders/:id', (req, res) => {
+        console.log(req.body)
+        ordersCollection.insertOne(req.body).then((documents) => {
+            res.send(documents.insertedId);
+        });
+
+    })
+
+
+    // client.close();
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log('My server running at', port)
+})
---
+++ test.js
@@ -0,0 +1,65 @@
+const aaa = [
+    {
+        "id": 1,
+        "name": "PATAIYA CITY",
+        "img": "https://i.ibb.co/qC1S7Mn/pattaya-city.jpg",
+        "bookingFee": 5000,
+        "offer": "3 days 2 nights",
+        "desc": "Pattaya is a city on Thailand’s eastern Gulf coast known for its beaches. A quiet fishing village as recently as the 1960s, it’s now lined with resort hotels, high-rise condos, shopping malls, cabaret bars and 24-hour clubs. Nearby, hillside Wat Phra Yai Temple features an 18m-tall golden Buddha. The area also features several designer golf courses, some with views of Pattaya Bay."
+    },
+    {
+        "id": 2,
+        "name": "Raffles Hotel Singapore",
+        "img": "https://i.ibb.co/9VrY7pH/1200x-1.jpg",
+        "bookingFee": 6000,
+        "offer": "3 days 3 nights",
+        "desc": "Featuring high ceilings and ornate furnishings, the posh suites have a range of amenities such as marble bathrooms, living rooms, flat-screen TVs and DVD players, as well as verandas and free Wi-Fi. Butler and room service are both available 24/7."
+    },
+    {
+        "id": 3,
+        "name": "Sydney Opera House",
+        "img": "https://i.ibb.co/SsM2Zwb/Opera-House-755d893182dc4811b608eb1a99792fd7.jpg",
+        "bookingFee": 7000,
+        "offer": "2 days 2 nights",
+        "desc": "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings"
+    },
+    {
+        "id": 4,
+        "name": "Walt Disney World Resort ",
+        "img": "https://i.ibb.co/zhxH8Ky/untitled-7.jpg",
+        "bookingFee": 5000,
+        "offer": "3 days 2 nights",
+        "desc": "The Walt Disney World Resort, also called Walt Disney World or Disney World, is an entertainment resort complex in Bay Lake and Lake Buena Vista, Florida, United States, near the cities of Orlando and Kissimmee."
+    },
+    {
+        "id": 5,
+        "name": "Forbidden City ",
+        "img": "https://i.ibb.co/PGZRJ4r/shutterstock-721309024.jpg",
+        "bookingFee": 5800,
+        "offer": "3 days 2 nights",
+        "desc": "The Forbidden City (Chinese: 紫禁城; pinyin: Zǐjìnchéng) is a palace complex in Dongcheng District, Beijing, China, at the center of the Imperial City of Beijing. It is surrounded by numerous opulent imperial gard."
+    },
+    {
+        "id": 6,
+        "name": "Antigua & Barbuda",
+        "img": "https://i.ibb.co/s3QmFVq/opt-aboutcom-coeus-resources-content-migration-brides-public-brides-services-production-2016-11-03-5.webp",
+        "bookingFee": 6000,
+        "offer": "3 days ",
+        "desc": "Pastel facades, bustling markets, tropical fruits, limestone formations and, of course, plenty of pristine beaches: Antigua and Barbuda are essentially Eden. Between the two islands you can find yourselves alone on powdery white sand, or on a catamaran sailing between the Atlantic Ocean and Caribbean Sea at sunset."
+    },
+    {
+        "id": 7,
+        "name": "Bali, Indonesia",
+        "img": "https://i.ibb.co/qC1S7Mn/pattaya-city.jpghttps://i.ibb.co/SBbdh0H/bali-indonesia-0cb9ab9f6e85422283627acd11cea100.webp",
+        "bookingFee": 9000,
+        "offer": "4 days 3 nights",
+        "desc": "A honeymoon in Bali is pure magic. The Indonesian island—the only one in the huge country that’s Hindu—is a perfect place for passion. With a bit more of an exotic feel than the tropical islands closer to the US, Bali doesn’t only offer unspoiled beaches, surfing, snorkeling and massages, it’s full of culture, art, music, rice terraces and temples."
+    },
+    {
+        "id": 8,
+        "name": "Buenos Aires, Argentina",
+        "img": "https://i.ibb.co/Y38y2Y0/opt-aboutcom-coeus-resources-content-migration-brides-public-brides-services-production-2016-11-03-5.webp",
+        "bookingFee": 6700,
+        "offer": "If you venture to Argentina for your honeymoon, prepare to do the super sensual tango. The nightlife in the capital city is famously fabulous, and people come from all around the world to dance. Enjoy all the vibrant culture, local grill-focused cuisine and vivid, photogenic surroundings from a bicycle—bike lanes cover most of the city—or your temporary home in a sexy, well-designed place like Faena Hotel Buenos Aires or deluxe Four Seasons Hotel Buenos Aires."
+    }
+]
